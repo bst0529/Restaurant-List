5ee711562a6b25bfe3ddf85b44db2e49c8f45c85
Liu Chun-Kai <bst0529@hotmail.com>
2022-03-07 18:21:07 +0800

    feat: 初次建立專案

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+
+const restaurantList = require('./restaurant.json')
+
+app.engine('hbs',exphbs.engine({extname: '.hbs', defaultLayout: 'main.hbs'}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`Web is starting on http://localhost:${port}`)
+})
