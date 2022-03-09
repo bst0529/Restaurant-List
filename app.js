1ef1b40301ed8d6edcd8e886f83190ae3b0a8077
bst0529 <bst0529@hotmail.com>
2022-03-09 23:07:57 +0800

    feat: 餐廳清單改由 each 呈現

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'hbs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index',{restaurants: restaurantList.results})
 })
 
 app.get('/restaurants/:id', (req, res) => {
